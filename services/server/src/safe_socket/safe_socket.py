e1ebb016682cc9dfea879e6ca1653c779415cd37
Lucas Ariel Conde Cardó <conde.cardo.lucas@gmail.com>
2026-08-27 18:47:22 -0300

    Esquema para implementación de short read y short write en server

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -1,11 +1,33 @@
 import socket
 
-# TODO: Complete with a short-read/short-write tolerant implementation
-
 
 def recv_all(socket: socket.socket, size):
+    """
+    bytes = b''
+    remaining = size
+    while remaining != 0:
+        new_bytes = socket.recv(remaining)
+
+        if len(new_bytes) == 0:
+            break # Levantar error?
+
+        remaining -= len(new_bytes)
+        bytes += new_bytes
+    return bytes
+    """
     return socket.recv(size)
 
 
 def send_all(socket: socket.socket, bytes):
+    """
+    total_sent = 0
+    while total_sent < len(bytes):
+        sent = socket.send(bytes[total_sent:])
+
+        if sent == 0:
+            return total_sent # Levantar error?
+
+        total_sent += sent
+    return total_sent
+    """
     return socket.send(bytes)
